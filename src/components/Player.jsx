8d07b29a2a0afd9e3f566ac6909908b138f0a85b
waseem tasawar <waseemtasawar97@gmail.com>
2024-07-08 20:18:12 +0500

    Make a New Game

---
+++ src/components/Player.jsx
@@ -0,0 +1,20 @@
+import { useRef, useState } from "react";
+
+export default function Player() {
+  const playerName = useRef();
+
+  const [enteredName, setenteredName] = useState("");
+
+  function HandleClick() {
+    setenteredName(playerName.current.value);
+  }
+  return (
+    <section id="player">
+      <h2>Welcome {enteredName ? enteredName : "unknown entity"}</h2>
+      <p>
+        <input ref={playerName} type="text" />
+        <button onClick={HandleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
